d62a0cf3ceba6ab26839c9cce081bb41a7762eb1
satindersharma <47383766+satindersharma@users.noreply.github.com>
2023-05-09 16:26:45 +0530

    allow csv

--- importfile/forms.py
+++ importfile/forms.py
@@ -1,15 +1,19 @@
 from django import forms
-from django.core.exceptions import ValidationError
-import openpyxl
+from django.core.validators import FileExtensionValidator
+from pathlib import Path
 from io import BytesIO
 import pandas as pd
 
 
 class FarmerImport(forms.Form):
-    file = forms.FileField(required=False)
+    file = forms.FileField(required=False,validators=[FileExtensionValidator(['csv','xls','xlsx'])])
 
     def send_file(self):
         file = self.cleaned_data['file']
-
-        df =  pd.read_excel(file.read())
+        extension = Path(file.name).suffix[1:].lower()
+        if extension == 'csv':
+            df = pd.read_csv(BytesIO(file.read()))
+        else:
+            # assuming file is excel
+            df =  pd.read_excel(file.read())
         return df.to_html()
